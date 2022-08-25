9b531864ada95272a4fb30aaaa3605b01f4af2ad
Juan Albert Wibowo <juanalbert240303@gmail.com>
2022-08-25 19:24:31 +0700

    Add gitignore, procfile, and update ports

--- app.js
+++ app.js
@@ -96,6 +96,11 @@ app.get("/recommendations", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("App has started successfully.");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () => {
+  console.log("Server has started successfully.");
 });
